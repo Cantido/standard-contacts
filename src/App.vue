b5bfd2876a80857c8d380ecfcc66dc0fbd3903eb
Rosa Richter <robert.c.richter@gmail.com>
2018-12-08 13:38:48 -0700

    Strip all of the hello-world excess stuff

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Here's a <em>real</em> basic hello world.
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
 
