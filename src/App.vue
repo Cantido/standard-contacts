0b696b5683c023b906ef327011d8bd9e2ed84bf7
Rosa Richter <robert.c.richter@gmail.com>
2018-12-08 17:14:07 -0700

    Start printing info from a vcard

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <template>
   <div id="app">
-    Here's a <em>real</em> basic hello world.
+    <div>
+      <h2>Contact</h2>
+      Name: {{ fn }}
+    </div>
+    <div class="vcard">
+      <h2>VCARD</h2>
+      <span v-for="line in vlines">
+        {{ line }}<br />
+      </span>
+    </div>
+    <div class="">
+      <h2>JCARD</h2>
+      {{ jcard }}
+    </div>
   </div>
 </template>
 
 <script>
+import ICAL from 'ical.js';
+
 export default {
   name: 'app',
-  components: { }
+  data: () => {
+    let v = `
+BEGIN:VCARD\n
+VERSION:3.0\n
+PRODID:ez-vcard 0.10.4\n
+N:Anderson;Thomas\n
+FN:Thomas Anderson\n
+TEL;TYPE=CELL:+1 (555) 555-5555\n
+EMAIL;TYPE=HOME:neo@example.com\n
+END:VCARD\n
+`;
+
+      let j = ICAL.parse(v)
+      let jcomp = new ICAL.Component(j);
+
+
+
+    return {
+      vcard: v,
+      vlines: v.split('\n'),
+      jcard: j,
+      fn: jcomp.getFirstPropertyValue("fn")
+    }
+  }
 }
 </script>
 
@@ -16,8 +54,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 30%;
+}
+.vcard {
+  text-align: left;
 }
 </style>
