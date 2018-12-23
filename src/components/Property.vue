0239b48fd00305b791e885d512eb88a0a4131570
Rosa Richter <rosa.richter@protonmail.com>
2018-12-23 11:53:20 -0600

    Hide prodid and version fields

--- src/components/Property.vue
+++ src/components/Property.vue
@@ -1,6 +1,6 @@
 <template lang="html">
-<span class="property">
   <label :for="prop[0]">{{prop[0]}}</label>
+<span class="property" v-if="prop[0] != 'prodid' && prop[0] != 'version'">
   <input :id="prop[0]" :name="prop[0]" v-model="prop[3]">
 </span>
 </template>
